4e854d1e3513d0fc523105cb08c2c2550616165b
acastillo <acastillo@localhost.localdomain>
2019-06-11 00:24:15 -0500

    remove node_modules

--- node_modules/ai-agents/lib/index.js
+++
@@ -1,5 +0,0 @@
-const Problem = require('./core/Problem');
-const Agent = require('./core/Agent');
-const AgentController = require('./core/AgentController');
-
-module.exports = { Problem, Agent, AgentController };
